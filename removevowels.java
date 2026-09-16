10d3766aaf733111dbe11901637efd2330280689
Bhoomica R Gowda <bhoomicarg7@gmail.com>
2026-09-16 21:33:45 +0530

    java remove vowels program

---
+++ removevowels.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class removevowels{
+    public static void main(String args[]){
+        String st="you can do it";
+        removeVowels(st);
+
+    }
+     public static void removeVowels(String st){
+        for(int i=0;i<st.length();i++){
+            char ch=st.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                   continue;
+            }
+            System.out.print(ch);
+
+        }
+
+    } 
+    
+}
